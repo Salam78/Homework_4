919e25eb902359aa60c09c62e7ff7b9755adb276
Meiram <133873249+Salam78@users.noreply.github.com>
2023-05-31 18:23:48 +0600

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,26 @@
+def check_affordability(s, p, m):
+    return s + p <= m
+
+s = int(input("Введите стоимость подписки на онлайн-кинотеатр: "))
+p = int(input("Введите стоимость пиццы: "))
+m = int(input("Введите зарплату Пети: "))
+
+result = check_affordability(s, p, m)
+if result:
+    print("Да")
+else:
+    print("Нет")
+input("press any key to close window")
+# def check_affordability(s, p, m):
+#     if s + p <= m:
+#         return "Да"
+#     else:
+#         return "Нет"
+#
+# s = int(input("Введите стоимость подписки на онлайн-кинотеатр: "))
+# p = int(input("Введите стоимость пиццы: "))
+# m = int(input("Введите зарплату Пети: "))
+#
+# result = check_affordability(s, p, m)
+# print(result)
+# input("press any key to close window")
